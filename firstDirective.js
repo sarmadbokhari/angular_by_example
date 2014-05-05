3f895da6cf8d87a9d5eeca2b99a4c8a26b5d1d16
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-05 16:52:10 -0500

    Angular directive restrictions: element and attribute

--- firstDirective.js
+++ firstDirective.js
@@ -1,8 +1,21 @@
 var app = angular.module('superhero', []);
 
-app.directive('superman', function(){
+app.directive('supermanelement', function(){
   return{
     restrict: "E",
-    template: "<div>Here I am to save the day</div>"
+    template: "<div>Here I am to save the day as an element</div>"
   };
 });
+
+app.directive('supermanattribute', function(){
+  return{
+    restrict: "A",
+    link: function(){
+      alert("Here I am to save the day as an attribute");
+    }
+  };
+});
+
+// E = element
+// A = attribute
+// C = class
