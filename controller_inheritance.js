d3a08d9ff4aef2468f474350cb06de28c51169bc
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-05 14:37:10 -0500

    Angular filter to reverse input text

--- controller_inheritance.js
+++ controller_inheritance.js
@@ -4,14 +4,16 @@ myApp.factory('Data', function(){
   return {message:"I'm data from a service"};
 });
 
+myApp.filter('reverse', function(Data){
+  return function(text){
+    return text.split("").reverse().join("") + Data.message;
+  };
+});
+
 function FirstCtrl($scope, Data){
   $scope.data = Data;
 }
 
 function SecondCtrl($scope, Data){
   $scope.data = Data;
-
-  $scope.reversedMessage = function(message){
-    return message.split("").reverse().join("");
-  };
 }
