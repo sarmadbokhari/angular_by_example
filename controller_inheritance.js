233bebe92448f2abe938857d0173db46b4bf2fdb
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-05 13:55:42 -0500

    Sharing data between controllers

--- controller_inheritance.js
+++ controller_inheritance.js
@@ -1,7 +1,13 @@
-angular.module("myapp", [])
-  .controller("FirstCtrl", function($scope){
+var myApp = angular.module("myApp", []);
 
-  })
-  .controller("SecondCtrl", function($scope){
+myApp.factory('Data', function(){
+  return {message:"I'm data from a service"};
+});
 
-  });
+function FirstCtrl($scope, Data){
+  $scope.data = Data;
+}
+
+function SecondCtrl($scope, Data){
+  $scope.data = Data;
+}
