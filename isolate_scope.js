d062a452d6d90163433d0bfce3955758bb59c03f
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-06 14:03:17 -0500

    Passing task through scope to alert

--- isolate_scope.js
+++ isolate_scope.js
@@ -1,11 +1,19 @@
 var app = angular.module("taskApp", []);
 
+app.controller('TaskCtrl', function($scope){
+  $scope.logTask = function(task){
+    alert(task + " is done!");
+  };
+});
+
 app.directive("person", function(){
   return {
     restrict: "E",
     // this isolates the scope
-    scope:{},
-    template: '<input type="text" ng-model="task">{{ task }}'
+    scope:{
+      done: "&"
+    },
+    template: '<input type="text" ng-model="task">{{ task }} <div class="button" ng-click="done({task:task})">Task completed</div>'
   };
 });
 
