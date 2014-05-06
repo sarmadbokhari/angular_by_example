118abccb38c3555b12f503156fb986ed4d95039b
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-06 12:08:38 -0500

    Create a superhero directive as an element, and 3 directives as attributes for superman, flash, and hulk and console their powers

---
+++ directives_communicating_with_directives.js
@@ -0,0 +1,55 @@
+var app = angular.module('superApp', []);
+
+app.directive('superhero', function(){
+  return {
+    restrict: "E",
+    // this will keep it from overwriting, isolated scope
+    scope:{},
+    controller: function($scope){
+      $scope.abilities = [];
+      this.addStrength = function(){
+        $scope.abilities.push('strength');
+      };
+      this.addSpeed = function(){
+        $scope.abilities.push('speed');
+      };
+      this.addFlight = function(){
+        $scope.abilities.push('flight');
+      };
+    },
+    link: function(scope, element){
+      element.addClass('button');
+      element.bind('mouseenter', function(){
+        console.log(scope.abilities);
+      });
+    }
+  };
+});
+
+
+app.directive('strength', function(){
+  return {
+    require: "superhero",
+    link: function(scope, element, attrs, superheroCtrl){
+      superheroCtrl.addStrength();
+    }
+  }
+})
+
+app.directive('speed', function(){
+  return {
+    require: "superhero",
+    link: function(scope, element, attrs, superheroCtrl){
+      superheroCtrl.addSpeed();
+    }
+  }
+})
+
+app.directive('flight', function(){
+  return {
+    require: "superhero",
+    link: function(scope, element, attrs, superheroCtrl){
+      superheroCtrl.addFlight();
+    }
+  }
+})
