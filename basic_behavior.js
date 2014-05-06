f0eb1d5fcb8f72089877b8f3d5ca917a6e38f9be
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-06 10:34:11 -0500

    Mouseenter and mouseleave directive tests

--- basic_behavior.js
+++ basic_behavior.js
@@ -1,7 +1,17 @@
-var app = angular.module("behaviorApp", [])
+var app = angular.module("behaviorApp", []);
 
 app.directive("enter", function(){
-  return{
-    restrict: "A"
+  return function(scope, element){
+    element.bind("mouseenter", function(){
+      console.log("I'm inside of you");
+    });
+  };
+});
+
+app.directive("leave", function(){
+  return function(scope, element){
+    element.bind("mouseleave", function(){
+      console.log("I'm leaving you");
+    });
   };
 });
